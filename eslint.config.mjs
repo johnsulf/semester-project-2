45cb12c294420f5d6812353686fe76e4a6e93727
johnsulf <erlendjohns@gmail.com>
2024-09-02 12:14:10 +0200

    eslint broilerplate config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,40 @@
+import cypressPlugin from "eslint-plugin-cypress";
 import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import eslintRecommended from "@eslint/js";
 
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2021,
+        ...globals.jest,
+      },
+    },
+
+    plugins: {},
+    ...eslintRecommended.configs.recommended,
+
+    rules: {
+      "no-unused-vars": "error",
+    },
+  },
+  {
+    files: ["**/*.cy.js"],
+    languageOptions: {
+      globals: {
+        ...globals.cypress,
+      },
+    },
+    plugins: {
+      cypress: cypressPlugin,
+    },
+    rules: {
+      "cypress/no-unnecessary-waiting": "off",
+      "no-unused-vars": "off",
+    },
+  },
 ];
