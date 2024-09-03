9931a2004cffd667a238339b7271d5b2ef7497c5
johnsulf <erlendjohns@gmail.com>
2024-09-03 11:34:49 +0200

    adds jsdocs for storage functions

--- src/js/storage/load.js
+++ src/js/storage/load.js
@@ -1,3 +1,14 @@
+/**
+ * Load data from local storage
+ * @param {string} key - The key to load the data from
+ * @returns {any}
+ * @example
+ * const token = load('token');
+ * if (token) {
+ *  console.log(token);
+ * }
+ */
+
 export const load = (key) => {
   const data = localStorage.getItem(key);
   return data ? JSON.parse(data) : null;
--- src/js/storage/remove.js
+++ src/js/storage/remove.js
@@ -1,1 +1,10 @@
+/**
+ * Remove an item from local storage
+ * @param {string} key - The key to remove from local storage
+ * @returns {void}
+ * @example
+ * remove('token');
+ * console.log(load('token')); // null
+ */
+
 export const remove = (key) => localStorage.removeItem(key);
