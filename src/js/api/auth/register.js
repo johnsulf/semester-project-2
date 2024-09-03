ef40008c43810787f9ad9b55ff756cf070f630c4
johnsulf <erlendjohns@gmail.com>
2024-09-03 12:43:58 +0200

    adds unit test for register function

--- src/js/api/auth/register.js
+++ src/js/api/auth/register.js
@@ -2,6 +2,22 @@ import { apiBase } from '../constants.js';
 import { register as regEp } from '../endpoints.js';
 import { headers } from '../headers.js';
 
+/**
+ * Register a new user
+ * @param {string} name - The name of the user
+ * @param {string} email - The email of the user
+ * @param {string} password - The password of the user
+ * @param {object} avatar - The avatar of the user
+ * @returns {Promise} - The response
+ * @throws {Error} - The error
+ * @example
+ * register('Kalle_Kanin', 'kalle_kanin1234@stud.noroff.no', '12345678', {
+ *  url: 'https://www.erlendjohnsen.com/assets/images/profile.png',
+ *  alt: 'Profile image of Kalle Kanin',
+ * });
+ * @see {@link https://docs.noroff.dev/docs/v2/auth/register|Noroff API v2 | Auth | Register}
+ */
+
 export async function register(name, email, password, avatar) {
   try {
     const response = await fetch(`${apiBase + regEp}`, {
@@ -16,7 +32,7 @@ export async function register(name, email, password, avatar) {
   }
 }
 
-register('Kalle_Kanin', 'kalle_kanin1234@stud.noroff.no', '12345678', {
+/* register('Kalle_Kanin', 'kalle_kanin1234@stud.noroff.no', '12345678', {
   url: 'https://www.erlendjohnsen.com/assets/images/profile.png',
   alt: '',
-});
+}); */
---
+++ src/js/api/auth/register.test.js
@@ -0,0 +1,17 @@
+import { register } from './register';
+import 'jest-localstorage-mock';
+
+describe('register', () => {
+  test('should return a response', async () => {
+    const data = await register(
+      'Kalle_Kanin',
+      'kalle_kanin1234@stud.noroff.no',
+      '12345678',
+      {
+        url: 'https://www.erlendjohnsen.com/assets/images/profile.png',
+        alt: 'Profile image of Kalle Kanin',
+      },
+    );
+    expect(data).toBeDefined();
+  });
+});
