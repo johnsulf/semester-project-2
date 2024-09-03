de58aa9781ec8ce46b8b11664a875f91ce7cbbef
johnsulf <erlendjohns@gmail.com>
2024-09-03 10:48:09 +0200

    created API key

--- src/js/constants/constants.js
+++
@@ -1,6 +0,0 @@
-export const BASE_URL = 'https://v2.api.noroff.dev/';
-export const API_KEY_ENDPOINT = 'auth/create-api-key';
-export const LOGIN_ENDPOINT = 'auth/login';
-export const REGISTER_ENDPOINT = 'auth/register';
-export const ACCESS_TOKEN =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiRXJsZW5kIiwiZW1haWwiOiJlcmxlbmQuam9obnNlbkBzdHVkLm5vcm9mZi5ubyIsImlhdCI6MTcyNTI4NDEzOX0.-xz5qkM2wDs3IMwcJyWOMuu6gfdZNenYMp1IvU5yQls';
