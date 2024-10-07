d1ccbebda4f31e598a10ea40103804fe852c06b1
johnsulf <erlendjohns@gmail.com>
2024-10-07 10:44:09 +0200

    html cleanup

--- src/js/views/homeView.js
+++ src/js/views/homeView.js
@@ -5,7 +5,17 @@ export async function homeView(app) {
     <section>
       <h1 class="text-3xl font-bold">Welcome to the Auction Site!</h1>
       <p>Browse listings and start bidding on items.</p>
-      <div id="listings-container" class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+      <div 
+        id="listings-container" 
+        class="
+          grid 
+          grid-cols-1 
+          gap-x-6 
+          gap-y-10 
+          sm:grid-cols-2 
+          lg:grid-cols-3 
+          xl:grid-cols-4 
+          xl:gap-x-8">
         <p>Loading listings...</p>
       </div>
     </section>
