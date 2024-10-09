16aa4812377b82bb006c7c0d918d843a5e40f282
johnsulf <erlendjohns@gmail.com>
2024-10-09 09:40:05 +0200

    refactoring listingView: place bid event

---
+++ src/js/events/auction/placeBid.js
@@ -0,0 +1,10 @@
+export function initPlaceBidEvent(container, listingId) {
+  const placeBidButton = container.querySelector('#placeBidButton');
+  if (placeBidButton) {
+    placeBidButton.addEventListener('click', () => {
+      // Implement the logic to open the bid modal or prompt
+      // TODO: openBidModal(listingId);
+      console.log(`Place a bid on listing ID: ${listingId}`);
+    });
+  }
+}
