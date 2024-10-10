2bf7939a60198aa5890f7b26ba417e254c588433
johnsulf <erlendjohns@gmail.com>
2024-10-10 14:40:50 +0200

    improvement to API call

--- src/js/api/auction/createListing.js
+++ src/js/api/auction/createListing.js
@@ -3,19 +3,26 @@ import { listings } from '../endpoints.js';
 import { headers } from '../headers.js';
 
 export async function createListing(data) {
-  console.log(data);
   try {
     const response = await fetch(`${apiBase + listings}`, {
       method: 'POST',
       headers: headers('application/json'),
       body: JSON.stringify(data),
     });
-    if (response.ok) {
-      const result = await response.json();
-      console.log(result);
-      return result;
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(
+        errorData.errors
+          ? errorData.errors[0].message
+          : 'Failed to create listing',
+      );
     }
+
+    const result = await response.json();
+    return result.data;
   } catch (error) {
-    console.error(error);
+    console.error('API Error:', error);
+    throw error;
   }
 }
