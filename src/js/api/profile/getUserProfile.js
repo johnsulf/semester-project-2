573d2668ed26593d524da2d0a72dad4e431b2781
johnsulf <erlendjohns@gmail.com>
2024-10-18 10:44:13 +0200

    adds function for fetching profile

---
+++ src/js/api/profile/getUserProfile.js
@@ -0,0 +1,27 @@
+import { apiBase } from '../constants.js';
+import { headers } from '../headers.js';
+import { load } from '../../storage/load.js';
+
+export async function getUserProfile() {
+  const user = load('profile');
+
+  try {
+    const response = await fetch(`${apiBase}/auction/profiles/${user.name}`, {
+      method: 'GET',
+      headers: headers('application/json'),
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(
+        data.errors ? data.errors[0].message : 'Failed to fetch user profile',
+      );
+    }
+
+    return data.data;
+  } catch (error) {
+    console.error('Error fetching user profile:', error);
+    throw error;
+  }
+}
