c1d3c134d767a0015b5b3e7bd2d6282240edfedc
johnsulf <erlendjohns@gmail.com>
2024-10-23 12:06:29 +0200

    adding comments to api/profile

--- src/js/api/headers.js
+++ src/js/api/headers.js
@@ -1,18 +1,23 @@
 import * as storage from '../storage/index.js';
 import { API_KEY } from './constants.js';
 
+// Function to create headers for API requests
 export const headers = (contentType) => {
-  const token = storage.load('token');
-  const headers = {};
+  const token = storage.load('token'); // Load the token from local storage
+  const headers = {}; // Create an empty object for headers
 
+  // Add the API key to the headers
   headers['X-Noroff-API-Key'] = API_KEY;
 
+  // Add the content type to the headers if it exists
   if (contentType) {
     headers['Content-Type'] = contentType;
   }
 
+  // Add the authorization header if a token exists
   if (token) {
     headers['Authorization'] = `Bearer ${token}`;
   }
+
   return headers;
 };
