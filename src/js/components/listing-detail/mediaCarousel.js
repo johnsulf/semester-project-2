d891e8d67ae12cb17e26d1a7ac4e5d21fed8fc7e
johnsulf <erlendjohns@gmail.com>
2024-11-01 14:13:17 +0100

    adds color to img carousel in listing detail

--- src/js/components/listing-detail/mediaCarousel.js
+++ src/js/components/listing-detail/mediaCarousel.js
@@ -8,11 +8,11 @@ export function mediaCarouselComponent(listing) {
         <div class="relative mb-4">
           <img id="mainImage" src="${listing.media[0].url}" alt="${listing.media[0].alt}" class="w-full h-auto rounded transition-opacity duration-300">
           <!-- Left Arrow -->
-          <button id="prevImage" class="hidden md:block absolute left-0 top-1/2 transform -translate-y-1/2 bg-white bg-opacity-50 p-2 rounded-full">
+          <button id="prevImage" class="hidden md:block absolute left-0 top-1/2 transform -translate-y-1/2 bg-secondary text-white py-2 px-4 rounded-full">
             &#10094;
           </button>
           <!-- Right Arrow -->
-          <button id="nextImage" class="hidden md:block absolute right-0 top-1/2 transform -translate-y-1/2 bg-white bg-opacity-50 p-2 rounded-full">
+          <button id="nextImage" class="hidden md:block absolute right-0 top-1/2 transform -translate-y-1/2 bg-secondary text-white py-2 px-4 rounded-full">
             &#10095;
           </button>
         </div>
