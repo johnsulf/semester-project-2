908182d852a9592994629c83f6154d3af1dc1d6f
johnsulf <erlendjohns@gmail.com>
2024-11-02 13:44:26 +0100

    update avatar error handling and button state

--- src/js/components/profile/editAvatarFormComponent.js
+++ src/js/components/profile/editAvatarFormComponent.js
@@ -13,7 +13,7 @@ export function editAvatarFormComponent(modal) {
       <input type="url" name="avatarUrl" id="avatarUrl" class="mt-1 p-2 border rounded w-full" required>
     </div>
     <div class="flex justify-end">
-      <button type="submit" class="bg-primary text-white px-4 py-2 rounded">Update</button>
+      <button type="submit" id="updateBtn" class="bg-primary text-white px-4 py-2 rounded">Update</button>
     </div>
   `;
 
--- src/js/events/profile/submitForm.js
+++ src/js/events/profile/submitForm.js
@@ -1,8 +1,10 @@
 import { updateUserProfile } from '../../api/profile/updateUserProfile.js';
+import { disableButton, enableButton } from '../../helpers/buttonState.js';
 import { refreshUserData } from '../../helpers/refreshUserData.js';
 
 // Function to submit the edit avatar form
 export async function submitEditAvatarFormListener(form, modal) {
+  const updateAvatarBtn = form.querySelector('#updateBtn');
   // Listen for the form submission
   form.addEventListener('submit', async (event) => {
     event.preventDefault();
@@ -17,6 +19,13 @@ export async function submitEditAvatarFormListener(form, modal) {
       return;
     }
 
+    disableButton(
+      updateAvatarBtn,
+      'Updating Avatar...',
+      'bg-primary',
+      'bg-gray-400',
+    );
+
     try {
       // Call the API to update the user profile
       await updateUserProfile({ avatar: avatar });
@@ -26,10 +35,11 @@ export async function submitEditAvatarFormListener(form, modal) {
 
       // Close the modal
       modal.remove();
-
+      alert('Avatar updated successfully!');
       // Reload the profile view
       window.location.reload();
     } catch (error) {
+      enableButton(updateAvatarBtn, 'Update', 'bg-gray-400', 'bg-primary');
       alert(`Error updating avatar: ${error.message}`);
     }
   });
