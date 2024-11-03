430d7b4e28daf24c0bc737276fdf957d6d3b6773
johnsulf <erlendjohns@gmail.com>
2024-11-03 13:18:51 +0100

    guard on place a bid btn

--- src/js/events/auction/placeBid.js
+++ src/js/events/auction/placeBid.js
@@ -1,5 +1,5 @@
 import { modalComponent } from '../../components/common/modalComponent.js';
-import { profile } from '../../api/auth/authState.js';
+import { isLoggedIn, profile } from '../../api/auth/authState.js';
 import { bidOnListingFormComponent } from '../../components/listing-detail/bidOnListingFormComponent.js';
 
 // Function to add an event listener to the place bid button
@@ -9,16 +9,23 @@ export function placeBidEventListener(container, listing) {
   // If the button exists, add the event listener
   if (placeBidButton) {
     placeBidButton.addEventListener('click', () => {
-      const modal = modalComponent(); // Create a modal
-      const user = profile(); // Get the user profile
-      const form = bidOnListingFormComponent(user, listing, modal); // Create the form
+      if (!isLoggedIn()) {
+        alert('You must be logged in to place a bid.');
+        window.location.hash = '#/login';
+        return;
+      } else {
+        // If the user is not logged in, redirect to the login page
+        const modal = modalComponent(); // Create a modal
+        const user = profile(); // Get the user profile
+        const form = bidOnListingFormComponent(user, listing, modal); // Create the form
 
-      // Add the form to the modal content
-      const modalContent = modal.querySelector('.modal-content');
-      modalContent.appendChild(form);
+        // Add the form to the modal content
+        const modalContent = modal.querySelector('.modal-content');
+        modalContent.appendChild(form);
 
-      // Append the modal to the body
-      document.body.appendChild(modal);
+        // Append the modal to the body
+        document.body.appendChild(modal);
+      }
     });
   }
 }
