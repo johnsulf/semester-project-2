b70361c5782ebe28d568b538079562d056a4caf6
johnsulf <erlendjohns@gmail.com>
2024-11-03 10:12:30 +0100

    latest listings carousel loader

---
+++ src/js/components/loaders/listingCardLoader.js
@@ -0,0 +1,17 @@
+// Function to create a skeleton loader for a listing card
+export function listingCardLoader() {
+  const listingElement = document.createElement('div'); // Create the listing element
+  listingElement.classList.add('p-4', 'rounded-md', 'mx-1', 'bg-white'); // Add classes to the listing element
+
+  // Set the listing element inner HTML
+  listingElement.innerHTML = `
+      <div class="w-full h-64 overflow-hidden rounded-lg bg-gray-400 animate-pulse"></div>
+      <div class="mt-3 h-6 w-2/3 bg-gray-400 animate-pulse"></div>
+      <div class="mt-3 h-5 bg-gray-400 animate-pulse"></div>
+      <div class="mt-2 h-10 bg-gray-400 w-full rounded animate-pulse flex items-center justify-center">
+        <div class="h-4 w-2/4 bg-white"></div>
+      </div>  
+    `;
+
+  return listingElement;
+}
--- src/js/events/listings/initLatestListingsCarousel.js
+++ src/js/events/listings/initLatestListingsCarousel.js
@@ -1,12 +1,19 @@
 import { getLatestListings } from '../../api/auction/getLatestListings.js';
 import { carouselComponent } from '../../components/listings/carouselComponent.js';
+import { displayLatestListingsLoader } from '../../helpers/displayLoaders.js';
 
 // Function to initialize the latest listings carousel
 export async function initLatestListingsCarousel() {
   const carouselContainer = document.getElementById('latestListingsCarousel');
 
+  displayLatestListingsLoader(carouselContainer); // Display Skeleton Loaders
+
   try {
+    // Fetch the Listings
     const listings = await getLatestListings(10); // Get the latest 10 listings
+
+    // Remove Skeleton Loaders
+    carouselContainer.innerHTML = '';
 
     // Display a message if there are no listings
     if (listings.length === 0) {
@@ -14,10 +21,11 @@ export async function initLatestListingsCarousel() {
       return;
     }
 
-    // Create the carousel component and append it to the container
+    // Create and Append the Carousel Component
     const carousel = carouselComponent(listings);
     carouselContainer.appendChild(carousel);
   } catch (error) {
+    // Handle Errors and Remove Loaders
     carouselContainer.innerHTML = `<p class="text-error">Error loading latest listings: ${error.message}</p>`;
   }
 }
