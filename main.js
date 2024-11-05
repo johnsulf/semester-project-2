4952835282da4619153c889e6fd313383f142b38
johnsulf <erlendjohns@gmail.com>
2024-11-05 11:54:25 +0100

    renamed js folder

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import router from './src/js/router/index.js';
-import { buildNav } from './src/js/components/nav/nav.js';
+import router from './src/app/router/index.js';
+import { buildNav } from './src/app/components/nav/nav.js';
 
 // Initialize the application
 document.addEventListener('DOMContentLoaded', () => {
