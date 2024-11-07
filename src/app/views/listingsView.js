1ba31d47ad19fb72aeee84ac39bdb0d1a69a162f
johnsulf <erlendjohns@gmail.com>
2024-11-07 13:04:17 +0100

    renamed and relocated listings events

--- src/app/views/listingsView.js
+++ src/app/views/listingsView.js
@@ -1,7 +1,5 @@
-import { searchListings } from '../api/auction/searchListings.js';
-import { listingCardComponent } from '../components/common/listingCardComponent.js';
 import { bouncer } from '../components/common/loaders/bouncer.js';
-import { listingEnded } from '../helpers/bidOnListing.js';
+import { populateListings } from '../events/listings/populateListings.js';
 
 /**
  * Renders the listings view based on the search query.
@@ -42,29 +40,5 @@ export async function listingsView(app, query) {
     'searchResultsContainer',
   );
 
-  // Fetch and display search results
-  try {
-    const results = await searchListings(query);
-    bouncerContainer.remove();
-
-    // If no results are found, display a message
-    if (results.length === 0) {
-      searchResultsContainer.innerHTML = '<p>No results found.</p>';
-      return;
-    }
-
-    const sortedResults = results.sort((a, b) => {
-      return listingEnded(a) - listingEnded(b);
-    });
-
-    // Display each listing in a card
-    sortedResults.forEach((listing) => {
-      const listingCard = listingCardComponent(listing);
-      searchResultsContainer.appendChild(listingCard);
-    });
-  } catch (error) {
-    bouncerContainer.remove();
-    // Display an error message if the request fails
-    searchResultsContainer.innerHTML = `<p class="text-error">Error loading search results: ${error.message}</p>`;
-  }
+  populateListings(searchResultsContainer, bouncerContainer, query);
 }
